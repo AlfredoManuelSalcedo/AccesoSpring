123c7267fa23fdc8beb53d5e272d81d0038518f9
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-02-09 10:40:42 +0100

    ejer04 usando un find sencillo

---
+++ JPA_02/src/main/java/principal/ejer04.java
@@ -0,0 +1,20 @@
+package principal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Alumno;
+
+
+public class ejer04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("default");
+		EntityManager em = emf.createEntityManager();
+		Alumno alumno = em.find(Alumno.class,"1111");
+		System.out.println(alumno.getNombre());
+	}
+
+}
